02259b9901bc381fd7920d33a1a2d19be63504e1
Aritra-3-Roy <aritra.briliant@gmail.com>
2025-09-01 12:50:25 +0530

    05-Authentication Socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,17 @@
-"use client";
-import { authClient } from "@/lib/auth-client";
-import { useState } from "react";
-import {Button} from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { on } from "events";
-import { se } from "date-fns/locale";
-export default function Home() {
+import { auth } from "@/lib/auth";
+import HomeView from "@/modules/home/ui/views/home-view";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
-  const { data: session } = authClient.useSession() 
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = async () => {
-    await authClient.signUp.email({
-        email, // user email address
-        name, // user password -> min 8 characters by default
-        password, // user display name
-    }, {
-      onError: () => {
-        window.alert("Something went wrong, please try again.");
-      },
-      onSuccess: () => {
-        window.alert("User created successfully!");
-      }
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
   });
-  };
 
-  const onLogin = async () => {
-    await authClient.signIn.email({
-        email, // user email address
-        password, // user display name
-    }, {
-      onError: () => {
-        window.alert("Something went wrong, please try again.");
-      },
-      onSuccess: () => {
-        window.alert("User created successfully!");
-      }
-  });
-  };
-
-  if(session) {
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Welcome back, {session.user.name}!</p>
-        <Button onClick={() => authClient.signOut()} className="w-full">
-        Sign Out
-      </Button>
-      </div>
-    );
+  if (!session) {
+    redirect("/sign-in");
   }
 
-  return (
-    <div className="flex flex-col gap-y-10">
-      <div className="p-4 flex flex-col gap-y-4">
-      <Input
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onSubmit} className="w-full">
-        Create User
-      </Button>
-    </div>
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onLogin} className="w-full">
-        Login
-      </Button>
-    </div>
-    </div>
-  );
-}
+  return <HomeView />;
+};
+export default Page;
